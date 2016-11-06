e33fd5c7389d6004df74e10fd6f0c0ed73804825
Luke Skibinski <lsh@ccxx.cx>
2016-11-06 20:41:01 +0000

    test.sh now activates the venv before running

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@
 
 # try just using './.test.sh' for fasting testing 
 
+source install.sh # activate the venv
 #source .lint.sh  # run the linter. picks up any basic errors or code smells
 source .test.sh   # run the unit tests. not run if lint fails
 source .scrub.sh  # run the autopep8 tool to fix minor lint issues. not run if tests fail.
