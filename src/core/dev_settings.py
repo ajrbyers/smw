d2058d48896cc2c50651770b7a9147799cde7217
Luke Skibinski <lsh@ccxx.cx>
2016-11-06 20:17:00 +0000

    removed the default 'root' password. this will help travis ci out

--- src/core/dev_settings.py
+++ src/core/dev_settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rua',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': ''
     }
