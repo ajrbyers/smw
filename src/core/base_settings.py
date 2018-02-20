58f315d07d8c5658a44964350eca73a848788094
Stuart Jennings <stuart.jennings@ubiquitypress.com>
2018-02-20 10:31:55 +0000

    Bump version: 1.2.3 → 1.2.4

--- src/core/base_settings.py
+++ src/core/base_settings.py
@@ -176,7 +176,7 @@ TEMPLATES = [
 
 # Updated, committed and tagged using 'bumpversion [major | minor | patch]'
 # run on master branch
-RUA_VERSION = '1.2.3'
+RUA_VERSION = '1.2.4'
 
 
 # ## LOGGING ##
