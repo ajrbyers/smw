0b4548685d59a03db829e20fa65069329370bc04
rowan08 <g07h3212@gmail.com>
2018-06-13 11:11:44 +0100

    Bump version: 1.7.3 → 1.7.4

--- src/core/base_settings.py
+++ src/core/base_settings.py
@@ -177,7 +177,7 @@ TEMPLATES = [
 
 # Updated, committed and tagged using 'bumpversion [major | minor | patch]'
 # run on master branch
-RUA_VERSION = '1.7.3'
+RUA_VERSION = '1.7.4'
 
 
 # ## LOGGING ##
