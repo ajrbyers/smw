34cc57c8aee48ab905d82ac0e3f3afed9f5a476b
Simon Crowe <info@simoncrowe.net>
2018-08-17 19:45:51 +0100

    Bump version: 1.8.2 → 1.9.0

--- src/core/base_settings.py
+++ src/core/base_settings.py
@@ -183,7 +183,7 @@ TEMPLATES = [
 
 # Updated, committed and tagged using 'bumpversion [major | minor | patch]'
 # run on master branch
-RUA_VERSION = '1.8.2'
+RUA_VERSION = '1.9.0'
 
 
 # ## LOGGING ##
